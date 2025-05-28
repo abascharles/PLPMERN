7b7806ad3cc5bfdb7f948d003f09ebfe5f09d378
abascharles <abascharlesbenard@gmail.com>
2025-05-28 16:43:51 +0300

    Connect to Mongo DB

--- week1/book-management/db.js
+++ week1/book-management/db.js
@@ -1,3 +1,4 @@
+//Connect to Mongo DB
 const { mongoClient } = require("mongodb");
 const url = "mongodb://localhost:27017";
 const client = new mongoClient(url);
@@ -6,3 +7,6 @@ async function conectDB() {
   await client.connect();
   return client.db("bookstore").collection("books");
 }
+
+// exporting in common js
+module.exports = connectDB;
