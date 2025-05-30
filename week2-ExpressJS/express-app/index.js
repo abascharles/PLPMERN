2e22a906d3ab9399017f4b2843ab35be94c172c6
abascharles <abascharlesbenard@gmail.com>
2025-05-30 11:29:55 +0300

    routes

--- week2-ExpressJS/express-app/index.js
+++ week2-ExpressJS/express-app/index.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
