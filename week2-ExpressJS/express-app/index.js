edf990ec3f8e76db586450e3673142fe134bf6d8
abascharles <abascharlesbenard@gmail.com>
2025-06-04 15:35:41 +0300

    fixed a typo

--- week2-ExpressJS/express-app/index.js
+++ week2-ExpressJS/express-app/index.js
@@ -4,7 +4,7 @@ const app = express(); //Initializing the app
 const port = 3000;
 
 app.get("/", (req, res) => {
-  req.send("Hello World"); //Sends a response to the client
+  res.send("Hello World"); //Sends a response to the client
 });
 
 //Start the server on port 3000
