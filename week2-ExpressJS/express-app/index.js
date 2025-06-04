6677b2b671c331d5359b99fdfd48ced7339d6a95
abascharles <abascharlesbenard@gmail.com>
2025-06-04 15:32:20 +0300

    Iteration 1

--- week2-ExpressJS/express-app/index.js
+++ week2-ExpressJS/express-app/index.js
@@ -1,19 +1,14 @@
-//import
+//Import express
 const express = require("express");
-
-//initialize app
-const PORT = 3000;
-const app = express();
+const app = express(); //Initializing the app
+const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  req.send("Hello World"); //Sends a response to the client
 });
 
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-});
+//Start the server on port 3000
 
-app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(port, () => {
+  console.log("Server is running of http://localhost:3000");
 });
