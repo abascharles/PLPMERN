bd8dc4cf963ce41023ca71a23674cae6876e21d8
abascharles <abascharlesbenard@gmail.com>
2025-06-05 08:58:41 +0300

    Setting up express js

--- week2-ExpressJS/express-app/index.js
+++ week2-ExpressJS/express-app/index.js
@@ -9,8 +9,37 @@ app.get("/", (req, res) => {
   res.send("Hello World"); //Sends a response to the client - outgoing resposnse  to the client
 });
 
-//Start the server on port 3000
+//Middlewares
+app.use((req, res, next) => {
+  console.log(`${req.method}  ${req.url}`);
+  next();
+});
 
+//Routing
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.post("/contact", (req, res) => {
+  res.send("Contact page");
+});
+
+//Route Parameter
+app.get("/user/:id", (req, res) => {
+  res.send(`User ID: ${req.params.id}`);
+});
+
+//req.params - object that holds all the route parameters
+
+//Query Parameter
+app.get("/search", (req, res) => {
+  res.send(`Serch Query: ${req.query.q}`); //respond with the qeury value
+});
+
+//qewry looks like this
+//search?q=value
+
+//Start the server on port 3000
 app.listen(port, () => {
   console.log("Server is running of http://localhost:3000");
 });
