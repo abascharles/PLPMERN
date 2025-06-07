b54d95e05aa494a290673bacc291330b70b85bd8
abascharles <abascharlesbenard@gmail.com>
2025-06-07 11:42:06 +0300

    delete

--- week2-ExpressJS/task-manager/routes.js
+++ week2-ExpressJS/task-manager/routes.js
@@ -44,6 +44,16 @@ router.put("/tasks/:id", async (req, res) => {
     });
 
     if (!task) return res.status(404).send();
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//delete a task
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).send();
     res.send(task);
   } catch (error) {
     res.status(500).send(error);
