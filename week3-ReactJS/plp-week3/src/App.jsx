eeba0d392a4dd55a95bc8a7ff6fb23cbc2731869
abascharles <abascharlesbenard@gmail.com>
2025-06-18 09:03:32 +0300

    Matering react through project

--- week3-ReactJS/plp-week3/src/App.jsx
+++ week3-ReactJS/plp-week3/src/App.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
-import Task from "./conponents/Task";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export default function App() {
-  conast[(tasks, setTasks)] = useState([
-    { id: 1, title: "Write Lesson Plan", completed: false },
-    { id: 2, title: "review Mongo db Quiz", completed: true },
-  ]);
-
-  const toggleTasky = (id) => {
-    setTasks((curr) =>
-      curr.map((t) => (t.id === id ? { ...t, completed: !t.completed } : t))
-    );
-  };
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl  font-bold mb-4">Task Dashboard</h1>
-      {}
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-//11:50
+export default App
