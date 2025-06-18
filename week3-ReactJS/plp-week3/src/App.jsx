cc24ab4b251eba6ceb811e926b486e83007b4ac2
abascharles <abascharlesbenard@gmail.com>
2025-06-18 13:14:30 +0300

    Creating task component

--- week3-ReactJS/plp-week3/src/App.jsx
+++ week3-ReactJS/plp-week3/src/App.jsx
@@ -1,9 +1,22 @@
-import "./index.css";
+import { useState } from 'react';
+import Task from './components/Task';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, title: 'Write Lesson Plan', completed: false },
+    { id: 2, title: 'Review MongoDB Quiz', completed: true },
+  ]);
+
+  const toggleTask = id => {
+    setTasks(curr => curr.map(t => (t.id === id ? { ...t, completed: !t.completed } : t)));
+  };
+
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-4">Task Dashboard</h1>
+      <h1 className="text-3xl font-bold mb-4"> Task dashboard</h1>
+      {tasks.map(task => (
+        <Task key={task.id} {...task} onToggle={toggleTask} />
+      ))}
     </div>
   );
 }
