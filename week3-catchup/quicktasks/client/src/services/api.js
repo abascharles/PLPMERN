a349b1e54a6bc6a5c07b791698dd70e7b55f4936
abascharles <abascharlesbenard@gmail.com>
2025-07-07 18:10:52 +0300

    feat: configuring axios

--- week3-catchup/quicktasks/client/src/services/api.js
+++ week3-catchup/quicktasks/client/src/services/api.js
@@ -1,1 +1,11 @@
 import axios from 'axios';
+
+//API base url
+const API = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+export const getTask = () => API.get('/tasks');
+export const createTask = task => API.post('/tasks', task);
+export const updateTask = (id, task) => API.put(`/tasks/${id}`, task);
+export const deleteTask = id => API.delete(`/tasks/${id}`);
