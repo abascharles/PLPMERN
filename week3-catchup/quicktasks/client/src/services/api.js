2e22c22262eb07d5eb344b0e022793da4091e3e1
abascharles <abascharlesbenard@gmail.com>
2025-07-07 18:11:57 +0300

    more axios

--- week3-catchup/quicktasks/client/src/services/api.js
+++ week3-catchup/quicktasks/client/src/services/api.js
@@ -5,6 +5,7 @@ const API = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
+//Bridging the gap betweem backend and frontend
 export const getTask = () => API.get('/tasks');
 export const createTask = task => API.post('/tasks', task);
 export const updateTask = (id, task) => API.put(`/tasks/${id}`, task);
