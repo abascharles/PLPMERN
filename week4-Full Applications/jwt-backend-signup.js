7508913e5df25399f6768df4bb0239478d37ac67
abascharles <abascharlesbenard@gmail.com>
2025-07-09 12:23:54 +0300

    Protecting routes using JWT middleware

--- week4-Full Applications/jwt-backend-signup.js
+++ week4-Full Applications/jwt-backend-signup.js
@@ -4,14 +4,37 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 
-app.post('/api/users', async (req, res) => {
+//registering user and generating a token
+
+app.post('/api/users/sigup', async (req, res) => {
   const { email, password } = req.body;
   //hashing passwod using bcrypt
   const hash = await bcrypt.hash(password, 10);
   //Creating user and indicating thatit is hashed.
   const user = await User.create({ email, password: hash });
 
-  //token generartion
+  //token generartion using jwt.sign() method
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
   res.json({ token });
 });
+
+//fectching the user
+
+app.post('/api/users/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(404).send('User not found');
+
+  //comparing the password after finding user(using bcrypt compare)
+  const isMatch = await bcrypt.compare(req.body.password, user.password);
+  if (!isMatch) return res.status(401).send('Invalid password');
+
+  //Generate the token
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+});
+
+//Protecting routes using JWT middleware
+
+const auth = (req, res, next) => {
+  const token = req.headers.authorization?.split('')[1];
+};
