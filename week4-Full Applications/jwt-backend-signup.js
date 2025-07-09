26bdcc17a131eb71f38b501aac7244b257d7a990
abascharles <abascharlesbenard@gmail.com>
2025-07-09 10:36:21 +0300

    token generation using jwt.sign() method

--- week4-Full Applications/jwt-backend-signup.js
+++ week4-Full Applications/jwt-backend-signup.js
@@ -10,4 +10,8 @@ app.post('/api/users', async (req, res) => {
   const hash = await bcrypt.hash(password, 10);
   //Creating user and indicating thatit is hashed.
   const user = await User.create({ email, password: hash });
+
+  //token generartion
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
 });
