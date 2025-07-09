17e1b00406fe6561ccbdd3ae32bdb6b84a3e98cc
abascharles <abascharlesbenard@gmail.com>
2025-07-09 09:50:38 +0300

    Understood cors, authentication and authorization

--- week4-Full Applications/backend.js
+++ week4-Full Applications/backend.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.post('/api/users', async (req, res) => {
   const user = await User.create(req.body);
   res.json(user); //sends data back to react
 });
+
+//Authentication - verfication after registering
+//Autghorization - level acess
