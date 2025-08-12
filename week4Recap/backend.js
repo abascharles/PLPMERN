2c15b611ad81e028eb327d1e5d905f46e60e3d5a
abascharles <abascharlesbenard@gmail.com>
2025-08-12 09:36:40 +0300

    JWT- authentication

--- week4Recap/backend.js
+++ week4Recap/backend.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
 const app = express();
+app.use(express.json());
+
 app.post('api/users', async (req, res) => {
   const user = await User.create(req.body);
   res.json(user); //sends data back to react
--- week4Recap/jwt-backen-signup.js
+++ week4Recap/jwt-backen-signup.js
@@ -1,9 +1,14 @@
-const jwt = require('jsonwebtoken')
-
+const jwt = require('jsonwebtoken');
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
-app.post('api/users', async(req, res) => {
-    const {email, password} = req.body
-})
+app.post('api/users', async (req, res) => {
+  const { email, password } = req.body;
+  const hash = await bcrypt.hash(password, 10);
+  const user = await User.create({ email, password: hash });
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+  res, json({ token });
+});
