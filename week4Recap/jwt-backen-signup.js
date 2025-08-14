060e8f3ac7c061ffe1375b1df3a94d61e2bafb06
abascharles <abascharlesbenard@gmail.com>
2025-08-14 09:01:22 +0300

    Adding comments - on jwt signup

--- week4Recap/jwt-backen-signup.js
+++ week4Recap/jwt-backen-signup.js
@@ -4,11 +4,14 @@ const bcrypt = require('bcrypt');
 
 const app = express();
 
-app.post('api/users', async (req, res) => {
+//Hashing the  password and cretaing anew user with the  hashed password
+
+app.post('api/users/signup', async (req, res) => {
   const { email, password } = req.body;
   const hash = await bcrypt.hash(password, 10);
   const user = await User.create({ email, password: hash });
 
+  //Generating a token
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-  res, json({ token });
+  res.json({ token });
 });
