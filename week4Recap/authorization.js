88e9fe88fa6f3dd8eb88bdb378995f3cb4facc6d
abascharles <abascharlesbenard@gmail.com>
2025-08-21 16:13:59 +0300

    Moved authorization token to authorization.js

--- week4Recap/authorization.js
+++ week4Recap/authorization.js
@@ -1,3 +1,16 @@
+const auth = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1]; //Bearer <token>
+  if (!token) return res.status(401).send('No valid token provided');
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).send('Invalid Token');
+  }
+};
+
 const authorize = roles => {
   return (req, res, next) => {
     if (!roles.includes(req.user.roles)) {
