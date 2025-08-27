0a1d835d4380c899e64a8f23cabee4b21e4dfb69
abascharles <abascharlesbenard@gmail.com>
2025-08-27 15:00:25 +0300

    Addede comments

--- week4Recap/backend.js
+++ week4Recap/backend.js
@@ -5,5 +5,5 @@ app.use(express.json());
 
 app.post('api/users', async (req, res) => {
   const user = await User.create(req.body);
-  res.json(user); //sends data back to react
+  res.json(user); //sends new user data back to react
 });
